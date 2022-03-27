816ef0af7ad31ef236a2e78196a3a4bf3d132bd7
takahirom <takam.dev@gmail.com>
2022-03-27 16:12:13 +0900

    Detect actually recomposed functions

--- app/src/main/java/com/github/takahirom/compose_multimodule_stability/MainActivity.kt
+++ app/src/main/java/com/github/takahirom/compose_multimodule_stability/MainActivity.kt
@@ -5,33 +5,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.github.takahirom.compose_multimodule_stability.ui.theme.ComposemultimodulestabilityTheme
 import com.github.takahirom.model.Article
 import com.github.takahirom.model.Articles
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      ComposemultimodulestabilityTheme {
-        Composable1(
-          Articles(
-            listOf(
-              Article("Compose"),
-              Article("Stability"),
-            )
+      val mutableState = remember { mutableStateOf<Int>(0) }
+      LaunchedEffect(Unit) {
+        while (true) {
+          delay(1000)
+          mutableState.value = mutableState.value + 1
+        }
+      }
+      val articles = remember {
+        Articles(
+          listOf(
+            Article("Compose"),
+            Article("Stability"),
           )
+        )
+      }
+      ComposemultimodulestabilityTheme {
+        mutableState.value
+        Composable1(
+          articles
         )
         Composable2(
           UiModel(
-            Articles(
-              listOf(
-                Article("Compose"),
-                Article("Stability"),
-              )
-            )
+            articles
           )
         )
       }
@@ -41,6 +50,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Composable1(articles: Articles) {
+  println("Composable1")
   Text(text = "Hello $articles!")
 }
 
@@ -49,6 +59,7 @@ data class UiModel(val articles: Articles)
 
 @Composable
 fun Composable2(uiModel: UiModel) {
+  println("Composable2")
   Text(text = "Hello $uiModel!")
   uiModel.articles.articles.forEach {
     Composable3(it)
@@ -57,5 +68,6 @@ fun Composable2(uiModel: UiModel) {
 
 @Composable
 fun Composable3(article: Article) {
+  println("Composable3")
   Text(text = "Hello $article!")
 }
